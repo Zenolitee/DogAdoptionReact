2f68e65c6441d2aa6d85f361f15689878343a887
Zense <73891112+Zenolitee@users.noreply.github.com>
2023-11-16 02:47:53 +0800

    Update Home.js

--- dogadopt/src/components/Home.js
+++ dogadopt/src/components/Home.js
@@ -17,6 +17,7 @@ const Home = () => {
 
 
     <button type="button" class="focus:outline-none text-white bg-green-500 hover:bg-green-800 focus:ring-1 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 ml-[4rem] mt-[1rem]">Learn More</button>
+    
   </div>
     
   
